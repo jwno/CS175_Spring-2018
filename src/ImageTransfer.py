113c67f0bc52220d7eb2d711dcc11049918b6d8a
AlvieHuang <Alvinh7@uci.edu>
2018-06-14 18:22:54 -0700

    edit transfer & uploaded test files

--- src/ImageTransfer.py
+++ src/ImageTransfer.py
@@ -4,14 +4,14 @@ import shutil
 ##USER EDIT START##
 
 # Directorie of Textfile for Hand
-path_txt = r'C:\Users\JayNo\Desktop\data\test_hands.txt'
+path_txt = r'E:/hand_pose/Dataset/test_hands.txt'
 
 # Directory of hand data
-datapath = r'C:\Users\JayNo\Desktop\data\hands'
+datapath = r'E:/hand_pose/Dataset/hands/'
 
 # Python creates new Folder.
 # Give it a directory name: r'C:\Users\...\new_folder_directory_name'
-newdirectory = r'C:\Users\JayNo\Desktop\data\new_directory'
+newdirectory = r'E:/hand_pose/Dataset/testhands/'
 
 ##USER EDIT END##
 
@@ -22,7 +22,13 @@ if not os.path.exists(newdirectory):
 list_closed = []
 with open(path_txt, 'r') as f:
     for filename in f.read().splitlines():
+        # Move jpg
         filename = filename[filename.rfind('/')+1:]
+        path = os.path.join(datapath, filename)
+        shutil.move(path, newdirectory)
+
+        # move txt
+        filename = filename[:-3] + 'txt'
         path = os.path.join(datapath, filename)
         shutil.move(path, newdirectory)
 
